8a8cbc57d2f063c91d496d2951f5efbcc7d7277b
Abayomi Aluko <franicons@gmail.com>
2022-11-03 18:20:58 +0100

    deploy staging

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> This is a practice to get me started on CI.
         </p>
         <a
           className="App-link"
